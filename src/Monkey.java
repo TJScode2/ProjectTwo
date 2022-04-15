3c07b5e4f8ac6ec2b12db951f861a3725f4e4e16
Tucker Salminen <Tuckersalminen@yahoo.com>
2022-04-15 19:43:39 -0400

    constructor

--- src/Monkey.java
+++ src/Monkey.java
@@ -2,7 +2,16 @@
 public class Monkey extends RescueAnimal {	
 	//constructor
 	public Monkey(String name, String animalType, String gender, String age, String weight, String acquisitionDate, String acquisitionCountry, String trainingStatus, Boolean reserved, String inServiceCountry) {
-		// TODO Auto-generated constructor stub
+        setName(name);
+        setGender(gender);
+        setAge(age);
+        setWeight(weight);
+        setAcquisitionDate(acquisitionDate);
+        setAcquisitionLocation(acquisitionCountry);
+        setTrainingStatus(trainingStatus);
+        setReserved(reserved);
+        setInServiceCountry(inServiceCountry);
+
 	}
 
 }
