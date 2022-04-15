af94e7aac9ae3dcbcc3f30d9e19197b90e08c309
Tucker Salminen <Tuckersalminen@yahoo.com>
2022-04-15 19:38:11 -0400

    Add instance variables

--- src/Monkey.java
+++ src/Monkey.java
@@ -1,6 +1,20 @@
 
 public class Monkey extends RescueAnimal {
-
+	
+	// instance variables 
+	
+    private String name = "Joey";
+    private String animalType = "Monkey";
+    private String gender = "Male";
+    private String age = "9";
+    private String weight = "265";
+    private String acquisitionDate = "June 8th";
+    private String acquisitionCountry = "Cape Town";
+	private String trainingStatus = "Not well";
+    private boolean reserved = true;
+	private String inServiceCountry = "USA";
+	
+	//constructor
 	public Monkey() {
 		// TODO Auto-generated constructor stub
 	}
